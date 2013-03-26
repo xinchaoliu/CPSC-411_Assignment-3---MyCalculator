294dd14428bdbf118190dca4ddbe4bf281f8af64
Xinchao Liu <leo_liu@csu.fullerton.edu>
2013-03-26 07:44:22 -0700

    Done with coding
    Require README and comments

---
+++ MyCalculator/CalculatorModel.h
@@ -0,0 +1,13 @@
+//
+//  CalculatorModel.h
+//  MyCalculator
+//
+//  Created by Xinchao Liu on 3/26/13.
+//  Copyright (c) 2013 Xinchao Liu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CalculatorModel : NSObject
+
+@end
---
+++ MyCalculator/CalculatorModel.m
@@ -0,0 +1,13 @@
+//
+//  CalculatorModel.m
+//  MyCalculator
+//
+//  Created by Xinchao Liu on 3/26/13.
+//  Copyright (c) 2013 Xinchao Liu. All rights reserved.
+//
+
+#import "CalculatorModel.h"
+
+@implementation CalculatorModel
+
+@end
